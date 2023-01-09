e3567c4c491b38e7ad107ce18585200de1a73e21
martinpatrikov <mpatrikovjr@gmail.com>
2023-01-09 22:31:13 +0200

    Final gui changes

--- HabitTracker.java
+++ HabitTracker.java
@@ -2,30 +2,27 @@ import java.awt.*;
 
 import javax.swing.*;
 import javax.swing.table.*;
-import java.awt.event.*;
 import java.util.*;
 
 public class HabitTracker extends JFrame {
-    // method for the action of buttons DONE and MISSED
-    public static void checkingButtonsAction(String check, JComboBox<String> cb, DefaultTableModel model) {
+    public static void activityButtonsAction(String check, JComboBox<String> cb, DefaultTableModel model, JComboBox<String> dropDownMenu) {
         if(cb.getSelectedItem() != null){
             String chosenHabit = cb.getSelectedItem().toString();
 
-            // going through every row in the first column of the table
             for (int i = 0; i < model.getRowCount(); i++) {
 
                 if (model.getValueAt(i, 0).equals(chosenHabit)) {
-                    // going through every cell in the selected row
-                    int miss = 0;
+                    int miss = -1;
                     for (int j = 0; j < model.getColumnCount() - 1; j++) {
                         if(model.getValueAt(i, j + 1) == "-"){
                             miss = j;
                         }
-                        // finding the first empty cell of the row
                         if (model.getValueAt(i, j + 1) == null) {
                             model.setValueAt(check, i, j + 1);
                             if(j  >= 20 && check.equals("+") && j - miss > 20){
-                                JOptionPane.showMessageDialog(null, "You have already done this habit for 21 days in a row!");
+                                JOptionPane.showMessageDialog(null, "You have successfully build a new healthy habit!");
+                                dropDownMenu.removeItemAt(dropDownMenu.getSelectedIndex());
+                                model.removeRow(i);
                             }
                             break;
                         }
@@ -36,13 +33,12 @@ public class HabitTracker extends JFrame {
         }else{
             JOptionPane.showMessageDialog(new JFrame("temp"), "Please select a habit from the list");
         }
-//        storeTableModel(model);
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Habit Tracker");
-        frame.setSize(1300, 600);
-        frame.getContentPane().setBackground(Color.decode("#354f52")  );
+        frame.setSize(1400, 600);
+        frame.getContentPane().setBackground(Color.decode("#354f52"));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
@@ -60,16 +56,10 @@ public class HabitTracker extends JFrame {
         titlePanel.add(title);
         basePanel.add(titlePanel);
 
-        // main containers and Layouts
         JPanel mainPanel = new JPanel();
         basePanel.add(mainPanel);
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
-//        frame.add(mainPanel);
-        // add panel at the top for the title
-        // add the title
-//        mainPanel.add(titlePanel);
 
-        // using as border between tablePanel and the left frame
         JPanel leftBorderPanel = new JPanel();
         mainPanel.add(leftBorderPanel);
         leftBorderPanel.setBackground(Color.LIGHT_GRAY);
@@ -77,11 +67,10 @@ public class HabitTracker extends JFrame {
 
         JPanel tablePanel = new JPanel();
         mainPanel.add(tablePanel);
-        tablePanel.setPreferredSize(new Dimension(1100, 300));
+        tablePanel.setPreferredSize(new Dimension(1200, 300));
         tablePanel.setBackground(Color.LIGHT_GRAY);
         tablePanel.setLayout(new BoxLayout(tablePanel, BoxLayout.Y_AXIS));
 
-        // using as border between tablePanel and rightPanel
         JPanel rightBorderPanel = new JPanel();
         mainPanel.add(rightBorderPanel);
         rightBorderPanel.setBackground(Color.LIGHT_GRAY);
@@ -93,24 +82,14 @@ public class HabitTracker extends JFrame {
         rightPanel.setBorder(BorderFactory.createLineBorder(Color.black));
         rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
 
-        // left side
+        JLabel upperBorderLabel = new JLabel(" ");
+        tablePanel.add(upperBorderLabel);
 
-        // using as upper border of table
-//        JLabel habitTrackerLabel = new JLabel(" ");
-//        tablePanel.add(habitTrackerLabel);
-
-        // Table
-
-        // using DefaultTableModel for its methods like addRow(), addColumn()...
-        DefaultTableModel habitsTableModel = new DefaultTableModel();
-        JTable habitsTable = new JTable(habitsTableModel) {
-
-            // make the table cells not editable from user (only from buttons in rightPanel)
-            public boolean isCellEditable(int data, int colums) {
+        DefaultTableModel tableModel = new DefaultTableModel();
+        JTable habitsTable = new JTable(tableModel) {
+            public boolean isCellEditable(int data, int columns) {
                 return false;
             }
-
-            // set color of missed/done cells
             public Component prepareRenderer(TableCellRenderer r, int row, int column) {
 
                 Component cell = super.prepareRenderer(r, row, column);
@@ -121,139 +100,112 @@ public class HabitTracker extends JFrame {
                     } else if (value.equals("+")) {
                         cell.setBackground(Color.decode("#B3FFAE"));
                     } else {
-                        cell.setBackground(Color.decode("#F8FFDB"));
+                        cell.setBackground(Color.decode("#e9e9e9"));
                     }
                 } else {
-                    cell.setBackground(Color.white);
+                    cell.setBackground(Color.decode("#f8f4e6"));
                 }
                 return cell;
             }
 
         };
 
-        // disables selecting whole row when certain cell is clicked on
         habitsTable.setRowSelectionAllowed(false);
-        // disables column moving
         habitsTable.getTableHeader().setReorderingAllowed(false);
 
-        habitsTableModel.addColumn("Habits\\Days");
+        tableModel.addColumn("Habits\\Days");
         habitsTable.getColumnModel().getColumn(0).setMinWidth(300);
         DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
         centerRenderer.setHorizontalAlignment( JLabel.CENTER );
 
         for (int i = 0; i < 31; i++) {
-            habitsTableModel.addColumn(String.valueOf(i + 1));
+            tableModel.addColumn(String.valueOf(i + 1));
         }
         for (int i = 0; i < 31; i++) {
             habitsTable.getColumnModel().getColumn(i + 1).setMaxWidth(50);
             habitsTable.getColumnModel().getColumn(i + 1).setCellRenderer( centerRenderer );;
         }
-
-        // clicking on right button scrolls table to the right
-
-//        habitsTableModel.insertRow(0, new Object[] { "Habits" });
         tablePanel.add(new JScrollPane(habitsTable));
 
-        // using as down border of table
-        JLabel label = new JLabel(" ");
-        tablePanel.add(label);
+        JLabel downBorderLabel = new JLabel(" ");
+        tablePanel.add(downBorderLabel);
 
-        // right side
-
-        // for layout setting
         rightPanel.setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(5, 5, 5, 5);
+        GridBagConstraints gridConstraints = new GridBagConstraints();
+        gridConstraints.insets = new Insets(5, 5, 5, 5);
 
-        JLabel checkLabel = new JLabel("Choose habit to check: ");
+        JLabel checkLabel = new JLabel("Choose habit: ");
         checkLabel.setForeground(Color.WHITE);
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        gbc.gridwidth = 2;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        rightPanel.add(checkLabel, gbc);
+        gridConstraints.gridx = 1;
+        gridConstraints.gridy = 1;
+        gridConstraints.gridwidth = 2;
+        gridConstraints.fill = GridBagConstraints.HORIZONTAL;
+        rightPanel.add(checkLabel, gridConstraints);
 
-        // drop-down menu
-        ArrayList<String> dropDownMenuHabits = new ArrayList<String>();
+        ArrayList<String> menuHabits = new ArrayList<>();
 
-        JComboBox<String> dropDownMenu = new JComboBox<String>();
-        // add ArrayList to drop menu (combo box)
-        for (int i = 0; i < dropDownMenuHabits.size(); i++) {
-            dropDownMenu.addItem(dropDownMenuHabits.get(i));
-        }
-        // set layout
-        gbc.gridx = 1;
-        gbc.gridy = 2;
-        gbc.gridwidth = 2;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        rightPanel.add(dropDownMenu, gbc);
+        JComboBox<String> dropDownMenu = new JComboBox<>();
+
+        gridConstraints.gridx = 1;
+        gridConstraints.gridy = 2;
+        gridConstraints.gridwidth = 2;
+        gridConstraints.fill = GridBagConstraints.HORIZONTAL;
+        rightPanel.add(dropDownMenu, gridConstraints);
 
         JButton doneButton = new JButton("Done");
         doneButton.setBackground(Color.decode("#cad2c5"));
-        gbc.gridx = 2;
-        gbc.gridy = 3;
-        gbc.gridwidth = 1;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        rightPanel.add(doneButton, gbc);
+        gridConstraints.gridx = 2;
+        gridConstraints.gridy = 3;
+        gridConstraints.gridwidth = 1;
+        gridConstraints.fill = GridBagConstraints.HORIZONTAL;
+        rightPanel.add(doneButton, gridConstraints);
 
-        doneButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                checkingButtonsAction("+", dropDownMenu, habitsTableModel);
-            }
-        });
+        doneButton.addActionListener(e -> activityButtonsAction("+", dropDownMenu, tableModel, dropDownMenu));
 
         JButton missedButton = new JButton("Missed");
         missedButton.setBackground(Color.decode("#cad2c5"));
 
-        gbc.gridx = 1;
-        gbc.gridy = 3;
-        gbc.gridwidth = 1;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        rightPanel.add(missedButton, gbc);
+        gridConstraints.gridx = 1;
+        gridConstraints.gridy = 3;
+        gridConstraints.gridwidth = 1;
+        gridConstraints.fill = GridBagConstraints.HORIZONTAL;
+        rightPanel.add(missedButton, gridConstraints);
 
-        missedButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                checkingButtonsAction("-", dropDownMenu, habitsTableModel);
-            }
-        });
+        missedButton.addActionListener(e -> activityButtonsAction("-", dropDownMenu, tableModel, dropDownMenu));
 
-        JLabel addLabel = new JLabel("Write habit to add: ");
+        JLabel addLabel = new JLabel("New habit:");
         addLabel.setForeground(Color.WHITE);
-        gbc.gridx = 1;
-        gbc.gridy = 4;
-        gbc.gridwidth = 2;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        rightPanel.add(addLabel, gbc);
+        gridConstraints.gridx = 1;
+        gridConstraints.gridy = 4;
+        gridConstraints.gridwidth = 2;
+        gridConstraints.fill = GridBagConstraints.HORIZONTAL;
+        rightPanel.add(addLabel, gridConstraints);
 
         JTextField addHabit = new JTextField();
-        gbc.gridx = 1;
-        gbc.gridy = 5;
-        gbc.gridwidth = 2;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        rightPanel.add(addHabit, gbc);
+        gridConstraints.gridx = 1;
+        gridConstraints.gridy = 5;
+        gridConstraints.gridwidth = 2;
+        gridConstraints.fill = GridBagConstraints.HORIZONTAL;
+        rightPanel.add(addHabit, gridConstraints);
 
-        JButton addButton = new JButton("Add habit");
+        JButton addButton = new JButton("Add");
         addButton.setBackground(Color.decode("#cad2c5"));
 
-        gbc.gridx = 1;
-        gbc.gridy = 6;
-        gbc.gridwidth = 2;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        rightPanel.add(addButton, gbc);
+        gridConstraints.gridx = 1;
+        gridConstraints.gridy = 6;
+        gridConstraints.gridwidth = 2;
+        gridConstraints.fill = GridBagConstraints.HORIZONTAL;
+        rightPanel.add(addButton, gridConstraints);
 
-        addButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String habitToAdd = addHabit.getText();
-                habitsTableModel.insertRow(habitsTableModel.getRowCount(), new Object[] { habitToAdd });
-                dropDownMenu.addItem(habitToAdd);
-                dropDownMenuHabits.add(habitToAdd);
-                addHabit.setText(null);
+        addButton.addActionListener(e -> {
+            String habitToAdd = addHabit.getText();
+            tableModel.insertRow(tableModel.getRowCount(), new Object[] { habitToAdd });
+            dropDownMenu.addItem(habitToAdd);
+            menuHabits.add(habitToAdd);
+            addHabit.setText(null);
 
-            }
         });
 
         frame.setVisible(true);
-
     }
-
 }
